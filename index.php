f06e62c020c0558764f05d9b31124416ac25fc39
rotem-bendavid <bendarotem@gmail.com>
2022-12-11 11:22:43 +0200

    Create a popup login page

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 
 <html lang="en">
@@ -6,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>PHP Sandbox</title>
+  <title>Login</title>
   <meta name="description" content="PHP sandbox">
   <meta name="author" content="MMD">
 
@@ -15,7 +14,24 @@
 </head>
 
 <body>
-  <div id="id-results" ></div>
-  <script src="scripts.js"></script>
+  <div class="container">
+    <button class="c-login-button" onclick="openForm()">Login </button>
+    <div class="c-form-popup" id="id-loginForm">
+    <form class="c-form-container" id="id-form">
+        <h2>LOGIN</h2>
+        <div>User Name</div>
+        <input type="text" id="id-username" placeholder="User Name" ><br>
+        <div>Password</div>
+        <input type="password" id="id-password" placeholder="Password" ><br> 
+        <div id="id-login-status"></div><p>
+        <button type="submit" id="id-submit-login" >Login</button>
+        <button type="button" onclick="closeForm()">Close</button>
+      </form>
+    </div>
+
+    <h2>Last task API</h2>
+    <div id="id-results" ></div>
+    <script src="scripts.js"></script>
+  </div>
 </body>
 </html>
--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ async function displayItems () {
     let response = await fetchData(); // Call to api
     response = filterLetter(response, 'e'); // Filter letters with 'e' in the beginning of title
     
-    // Building the view
+    // Building the API view
     response.forEach(item => {
         const listItem = document.createElement("details");
         listItem.classList.add('c-list-item');
@@ -28,8 +28,13 @@ async function displayItems () {
 
 // Call to api and returns array of objects from api
 async function fetchData() {
-    const response = await fetch(API_URL)
-    return await response.json();
+    try {
+        const response = await fetch(API_URL)
+        return await response.json();
+    }
+    catch (error) {
+        console.error(`Could not get it: ${error}`);
+    }
 }
 
 // Gets array and letter, returns filtered array of objects with the letter in the beginning of title
@@ -41,4 +46,62 @@ function filterLetter(response, letter) {
 }
 
 // First call to main function
-displayItems()
+displayItems();
+
+// Open login form popup
+function openForm() {
+    document.getElementById("id-loginForm").style.display = "block";
+}
+
+// Close login form popup
+function closeForm() {
+    document.getElementById("id-loginForm").style.display = "none";
+}
+
+// Event listener for login 
+var loginForm = document.getElementById('id-form');
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    userLogin();
+  });
+
+// Send user login data to user.rpc.php using fetchLogin and shows if login success
+async function userLogin() {
+    const username = document.getElementById('id-username').value;
+    const password = document.getElementById('id-password').value;
+
+    const params = {
+        username: username,
+        password: password, 
+    };
+    loginStatus = await fetchLogin(params);
+
+    // Shows if login success
+    if (!loginStatus.status) {
+        document.getElementById("id-login-status").style.color="red";
+    }
+    else {
+        document.getElementById("id-login-status").style.color="green";
+        closeForm();
+    }
+    document.getElementById("id-login-status").innerHTML = loginStatus.data;
+}
+
+// Send user login data to user.rpc.php
+async function fetchLogin(params) {
+    try {
+        const response = await fetch('rpc/user.rpc.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(params),
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error(`Could not get it: ${error}`);
+    }
+}
