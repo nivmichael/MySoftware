b2ef4003ee34b195b66d5aabce034303ae245b07
albert <albert@makemydayapp.com>
2023-05-02 10:13:58 +0300

    display 2 items per row

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
   <meta name="description" content="PHP sandbox">
   <meta name="author" content="MMD">
 
-  <link rel="stylesheet" href="styles.css?v=1.0">
+  <link rel="stylesheet" href="styles.css">
 
   <script type="text/javascript" src="js/nanoajax.min.js"></script>
     
 </head>
 
 <body>
-  <div id="results">
-    
+  <div id="results" class="container">
+
   </div>
   <script src="scripts.js"></script>
 </body>
--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@ nanoajax.ajax({url:'https://jsonplaceholder.typicode.com/posts'}, function (code
     var nHTML = '';
 
     for (var i = 0; i < ans.length; i++) {
-        nHTML += '<div class="blogs">' + ans[i]['title'];
+        nHTML += '<div class="item">' + ans[i]['title'];
         nHTML += '<br>';
         nHTML += '<p id="' + i + '" style="display: none;">' + ans[i]['body'] + '</p>';
-        nHTML += '<button onclick="showBody(' + i + ')">Click me</button>';
+        nHTML += '<button onclick="showBody(' + i + ')">Show Body</button>';
         // nHTML += '<hr>';
         nHTML += '</div>'
      }
