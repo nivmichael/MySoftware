e583ee423b7a03b9a3513c5034f79e9cb44de0eb
albertiancu2 <albert.nico142@gmail.com>
2023-05-02 13:18:44 +0300

    created users table

--- changes.sql
+++ changes.sql
@@ -1,11 +1,19 @@
-CREATE DATABASE blog;
-
-use blod;
-
-CREATE TABLE table_name (
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(100),
-    password VARCHAR(100),
-    
-   
+CREATE TABLE `blog`.`new_table` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(100) NULL,
+  `password` VARCHAR(100) NULL,
+  `created_at` DATETIME NULL,
+  `last_login` DATETIME NULL,
+  PRIMARY KEY (`id`)
 );
+
+
+
+INSERT INTO users (username, password, created_at, last_login)
+VALUES ('a@yahoo.com', 'aaa', CURRENT_TIMESTAMP , '2023-05-02 12:34:02');
+
+
+
+ALTER TABLE users
+ADD address VARCHAR(100);
+
