709f517adf78f63bf26b38546670f233fd0a6ccc
Michael Niv <nivmichael@gmail.com>
2023-05-22 12:51:49 +0300

    CR and further tasks

--- js/user.js
+++ js/user.js
@@ -3,7 +3,7 @@ var user = function() {
     function loginUser(ev) {
         ev.preventDefault();
 
-        const url = '/rpc/user.rpc.php'
+        const url = '/rpc/user.rpc.php?action=loginUser'
         const body = JSON.stringify({ username:ev.target.elements.uname.value , password: ev.target.elements.passw.value  })
 
         nanoajax.ajax({url, body, method:'POST'}, function (code, responseText) { 
--- rpc/user.rpc.php
+++ rpc/user.rpc.php
@@ -4,7 +4,8 @@ require_once '../autoload.php';
 // Takes raw data from the request
 header("Content-Type: application/json"); 
 $json = file_get_contents('php://input');
-
+var_dump($_GET);
+var_dump($_GET['action']);
 // Converts it into a PHP object
 $data = json_decode($json);
 die($data->username);
@@ -16,4 +17,11 @@ die($data->username);
 // var_dump(json_decode(array_keys($_REQUEST)[0])->password );
 
 // die;
+
+// CR - add a switch case for action 
+// Add validation fo username & password - if one missing retur error "missing credentials" and show error beneath the login
+// Create users table
+// Create a login function in user.class.php
+// Validate agains the DB with mysqli
+// Make sure to sanitize the unam and pwd
 
