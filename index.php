63970d6806959b3b2f848bc59722e66c76bdbda9
MissDaizy <dianaukrainsky@gmail.com>
2023-05-24 16:57:22 +0300

    Started implementing Nanoajax POST call.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,23 +8,24 @@
   <title>PHP Sandbox</title>
   <meta name="descrption" content="PHP sandbox">
   <meta name="authori" content="MMD">
-
   <link rel="stylesheet" href="styles.css?v=1.0">
-    
+
+  <script src="nanoajax.min.js"></script>
+
+<script src="scripts.js"></script>
 
 
-  <script src="scripts.js"></script>
   </head>
 
-<form>
+<form id="userLoginForm" onsubmit="userLogin(event)">
   <label for="username">Username:</label><br>
   <input type="text" id="username" name="username"><br>
   <label for="pwd">Password:</label><br>
   <input type="password" id="pwd" name="pwd">
+  <input type="submit" id="submitButton" value="Submit"> </input> 
+
 </form>
 <br>
-<button type="button" id="submitButton">Submit </button> 
 
-<body>
-</body>
+
 </html>
--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-//import NanoAjax from './js/nanoajax-master';
+function userLogin(event) {
+  event.preventDefault();
+  //console.dir(event.target.username.value)
+  console.log(event);
+  console.dir(event);
+  var req = { username: event.target.value };
+  var request = new XLHttpRequest();
 
-window.addEventListener("load", (event)=>{
-    var username  = document.getElementById("username");
-    var password = document.getElementById("pwd");
-    var submitButtom = document.getElementById("submitButton")
-    
-    submitButtom.addEventListener("click", userLogin(username,password))
-});
+  console.log( JSON.stringify(event.target.username.value))
+  nanoajax.ajax(
+    {
+      url: "/rpc/user.rpc.php",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: req,
+    },
+    function (code, responseText, request) {
+      console.log(code);
+      console.log(request);      
+    }
+  );
 
 
-function userLogin(username,password) {
-    console.log('userLogin function')
+
+
+
+
 }
